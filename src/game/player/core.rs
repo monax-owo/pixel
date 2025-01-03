ca67fe8e317ad1619f7cb74ac90a7df710366006
monax-owo <zprl.dev@gmail.com>
2025-01-03 15:35:23 +0900

    add collider

--- src/game/player/core.rs
+++ src/game/player/core.rs
@@ -1,5 +1,5 @@
 use bevy::{color::palettes::css, prelude::*};
-// use bevy_rapier2d::prelude::*;
+use bevy_rapier2d::prelude::*;
 // use blenvy::{BluePrintBundle, BlueprintInfo};
 
 #[derive(Component, Reflect, Debug)]
@@ -15,6 +15,16 @@ pub(super) fn setup(
     PbrBundle {
       mesh: meshes.add(Capsule3d::new(0.1, 0.2)),
       material: materials.add(Color::Srgba(css::MAGENTA)),
+      ..default()
+    },
+    Collider::capsule_y(0.1, 0.1),
+    RigidBody::KinematicVelocityBased,
+    KinematicCharacterController {
+      up: Vec2::Y,
+      offset: CharacterLength::Absolute(0.001),
+      snap_to_ground: Some(CharacterLength::Absolute(0.04)),
+      max_slope_climb_angle: 45_f32.to_radians(),
+      min_slope_slide_angle: 30_f32.to_radians(),
       ..default()
     },
   ));
