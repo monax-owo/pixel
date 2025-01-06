23b1b6f295052f67f699970dc10845ee3ffbcfbd
monax-owo <zprl.dev@gmail.com>
2025-01-06 12:05:11 +0900

    create DebugStore

--- src/dev/core.rs
+++ src/dev/core.rs
@@ -1,1 +1,7 @@
+use bevy::prelude::*;
 
+#[derive(Resource, Reflect, Default, Debug)]
+#[reflect(Resource)]
+pub struct DebugStore {
+  pub sample: bool,
+}
--- src/dev/plugin.rs
+++ src/dev/plugin.rs
@@ -1,9 +1,13 @@
 use bevy::prelude::*;
 
+use super::core::DebugStore;
+
 pub struct DevPlugin;
 
 impl Plugin for DevPlugin {
   fn build(&self, app: &mut App) {
-    let _ = app;
+    app
+      .init_resource::<DebugStore>()
+      .register_type::<DebugStore>();
   }
 }
