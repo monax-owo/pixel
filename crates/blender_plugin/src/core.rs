ebf1f6df6107a93700981e6e3a1154437d615eb2
monax-owo <zprl.dev@gmail.com>
2025-01-09 17:44:20 +0900

    change return type

--- crates/blender_plugin/src/core.rs
+++ crates/blender_plugin/src/core.rs
@@ -8,13 +8,13 @@ pub struct BlenderParserHooks {
 }
 
 pub trait BlenderParserHook {
-  fn add_parser<F: Fn(Entity, HashMap<&str, Value>) -> ()>(&mut self, parser: F);
+  fn add_parser<F: Fn(Entity, HashMap<&str, Value>) -> Result<(), ()>>(&mut self, parser: F);
 }
 
 impl BlenderParserHook for App {
   // TODO: Entityより自由度の高いものを使用したい(EntityのもつComponentを取得できたらいいな)
 
-  fn add_parser<F: Fn(Entity, HashMap<&str, Value>) -> ()>(&mut self, parser: F) {
+  fn add_parser<F: Fn(Entity, HashMap<&str, Value>) -> Result<(), ()>>(&mut self, parser: F) {
     // TODO:parserをリソースに入れ、システムから自動的に呼び出されるようにする
     // self.insert_resource()
   }
